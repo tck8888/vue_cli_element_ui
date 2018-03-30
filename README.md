bee3e73953c7c7502f0230bfb4d3eba30b4d25d5
tck8888 <380054728@qq.com>
2018-03-30 13:37:57 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vue_cli_element_ui
+# vue-cli 与element-ui学习
+
+## vue-cli的环境搭建
+
+## 安装element-ui
+
+    npm i element-ui -S
+
+## 在main.js中配置使用
+
+    import ElementUI from 'element-ui';
+    import 'element-ui/lib/theme-chalk/index.css';
+
+    Vue.use(Element, { size: 'small' });
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
